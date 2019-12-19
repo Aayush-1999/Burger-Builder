3a7a88069ae0d26ea56a76d97d42453759cdd505
Aayush-1999 <aayushaggarwal2007@gmail.com>
2019-12-19 17:48:14 +0530

    Add button disbale support and styling

--- src/Components/UI/Button/Button.js
+++ src/Components/UI/Button/Button.js
@@ -3,6 +3,7 @@ import classes from './Button.css';
 
 const button=(props)=>(
     <button className={[classes.Button, classes[props.btnType]].join(' ')}
+        disabled={props.disable}
         onClick={props.clicked}>
         {props.children}
     </button>
