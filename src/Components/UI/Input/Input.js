eac4a8a21a61c3d18c667ead0dca7a3bafc8d5e1
Aayush-1999 <aayushaggarwal2007@gmail.com>
2019-12-19 17:47:41 +0530

    Add validation error message

--- src/Components/UI/Input/Input.js
+++ src/Components/UI/Input/Input.js
@@ -2,19 +2,26 @@ import React from 'react';
 import classes from './Input.css';
 const input=(props)=>{
     let inputElement=null;
+    let validationError=null;
+    const inputClasses=[classes.InputElement];
 
+    if(props.invalid && props.shouldValidate && props.touched){
+        inputClasses.push(classes.Invalid);
+        validationError=<p className={classes.ValidateError}>Please Enter a valid value </p>
+    }
     switch(props.elementType){
         case 'input': 
-                inputElement=<input className={classes.InputElement} {...props.elementConfig} value={props.value} />;
+                inputElement=<input className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
                 break;
         case 'textarea' : 
-                inputElement=<textarea  className={classes.InputElement} {...props.elementConfig} value={props.value} />;
+                inputElement=<textarea  className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
                 break;
         case 'select' : 
                 inputElement=(
                             <select 
-                                className={classes.InputElement} 
-                                value={props.value}> 
+                                className={inputClasses.join(' ')} 
+                                value={props.value}
+                                onChange={props.changed} > 
                                 {props.elementConfig.options.map(option=>(
                                     <option key={option.value} value={option.value} >
                                         {option.displayValue}
@@ -29,6 +36,7 @@ const input=(props)=>{
         <div className={classes.Input}>
             <label className={classes.label} >{props.label}</label>
             {inputElement}
+            {validationError}
         </div>
     )
 }
