2c263d44c24c3db02af27b8667340af71f6e382e
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-01-27 23:18:05 +0530

    Add redux-thunk and advance support for redux dev tools

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore} from 'redux';
+import { createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
-import reducer from './store/reducers';
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import * as serviceWorker from './serviceWorker';
 
-const store=createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store=createStore(burgerBuilderReducer,composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 const app=(
     <Provider store={store} >
