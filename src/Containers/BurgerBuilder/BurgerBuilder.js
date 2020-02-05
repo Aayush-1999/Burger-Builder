c65b4b7e73b9c8c7f514160caaa09160681eda39
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-02-05 23:56:11 +0530

    Add init purchase and init ingredient, make loading global state variable for spinner

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -8,7 +8,7 @@ import OrderSummary from '../../Components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../Components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
-import * as burgerBuilderActions from '../../store/actions/index';
+import * as actions from '../../store/actions/index';
 
 class BurgerBuilder extends Component{
     state={
@@ -39,6 +39,7 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler=()=>{
+        this.props.onInitPurchase();
         this.props.history.push('/checkout');
     }
 
@@ -87,17 +88,18 @@ class BurgerBuilder extends Component{
 
 const mapStateToProps=state=>{
     return{
-        ings:state.ingredients,
-        price:state.totalPrice,
-        error:state.error
+        ings:state.burgerBuilder.ingredients,
+        price:state.burgerBuilder.totalPrice,
+        error:state.burgerBuilder.error
     };
 }
 
 const mapDispatchToProps=dispatch=>{
     return{
-        onIngredientAdded:(ingName)=> dispatch(burgerBuilderActions.addIngredient(ingName)),
-        onIngredientRemoved:(ingName)=> dispatch(burgerBuilderActions.removeIngredient(ingName)),
-        onInitIngredients:()=> dispatch(burgerBuilderActions.initIngredient())
+        onIngredientAdded:(ingName)=> dispatch(actions.addIngredient(ingName)),
+        onIngredientRemoved:(ingName)=> dispatch(actions.removeIngredient(ingName)),
+        onInitIngredients:()=> dispatch(actions.initIngredient()),
+        onInitPurchase:()=> dispatch(actions.purchaseInit)
     }
 }
 
