a6c7276962723aa3a54febb5ae12606e0170253f
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-02-07 00:30:30 +0530

    Add reducers for fetching orders

--- src/store/reducers/orders.js
+++ src/store/reducers/orders.js
@@ -24,12 +24,28 @@ const reducer=(state=initialState,action)=>{
                 id:action.orderId
             };
             return{
-            ...state,  
-            loading:false,
-            purchased:true,
-            orders:state.orders.concat(newOrder)
-        };
+                ...state,  
+                loading:false,
+                purchased:true,
+                orders:state.orders.concat(newOrder)
+            };
         case actionTypes.PURCHASE_BURGER_FAIL:
+            return{
+                ...state,
+                loading:false
+            };
+        case actionTypes.FETCH_ORDERS_START:
+            return{
+                ...state,
+                loading:true
+            };
+        case actionTypes.FETCH_ORDERS_SUCCESS:
+            return{
+                ...state,
+                orders:action.orders,
+                loading:false
+            };
+        case actionTypes.FETCH_ORDERS_FAIL:
             return{
                 ...state,
                 loading:false
