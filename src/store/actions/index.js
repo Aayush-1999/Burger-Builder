39d8c35840b416f371de144106c28ec60e5af6f4
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-02-07 00:30:11 +0530

    Add actions for fetch-orders

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,2 +1,2 @@
 export { addIngredient, removeIngredient, initIngredient } from './burgerBuilder';
-export { purchaseBurger, purchaseInit} from './order';
+export { purchaseBurger, purchaseInit , fetchOrders} from './order';
