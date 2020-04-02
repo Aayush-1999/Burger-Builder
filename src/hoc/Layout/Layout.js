9f7f8f0318b6b043a52de4f35e11998af8e255ba
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-04-03 03:03:12 +0530

    Add redux state for navigations component

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import classes from './Layout.css';
+import {connect} from 'react-redux';
 import Auxiliary from '../Auxiliary/Auxiliary';
 import Toolbar from '../../Components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../Components/Navigation/SideDrawer/SideDrawer';
@@ -22,8 +23,9 @@ class Layout extends Component{
     render(){
         return(
             <Auxiliary>
-                <Toolbar DrawerToggleClicked={this.drawerToggleClickedHandler} />
-                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler} />
+                <Toolbar isAuth={this.props.isAuthenticated} DrawerToggleClicked={this.drawerToggleClickedHandler} />
+                <SideDrawer isAuth={this.props.isAuthenticated}
+                open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler} />
                 <main className={classes.Content}>
                     {this.props.children}
                 </main>
@@ -32,4 +34,10 @@ class Layout extends Component{
     }
 }
 
-export default Layout;
+const mapStateToProps=(state)=>{
+    return{
+        isAuthenticated:state.auth.token!==null
+    }
+}
+
+export default connect(mapStateToProps)(Layout);
