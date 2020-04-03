60a0984ea37e6c92240209e4699cc44afc6aba0c
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-04-04 02:44:20 +0530

    Save userId in db on ordering burger and fetching user specific orders

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -62,11 +62,12 @@ export const fetchOrdersStart=()=>{
     };
 }
 
-export const fetchOrders=(token)=>{
+export const fetchOrders=(token,userId)=>{
     return dispatch =>{
         dispatch(fetchOrdersStart);
-        axios.get('/orders.json?auth='+token)
-        .then(res=>{
+        const queryParams='?auth='+token+'&orderBy="userId"&equalTo="'+userId+'"'
+        axios.get('/orders.json'+queryParams)
+        .then(res=>{ 
             const fetchedOrders=[];
             for(let key in res.data){
                 fetchedOrders.push({
