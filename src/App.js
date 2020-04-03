d4885d3290ba556c9b4b8302701026f5fbbfd687
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-04-04 02:42:24 +0530

    Fix refresh logout

--- src/App.js
+++ src/App.js
@@ -1,27 +1,63 @@
 import React,{Component} from 'react';
-import {Route} from 'react-router-dom';
-
+import {Route,Redirect,Switch,withRouter } from 'react-router-dom';
+import {connect} from 'react-redux';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './Containers/Checkout/Checkout';
 import Orders from './Containers/Orders/Orders';
 import Auth from './Containers/Auth/Auth';
 import Logout from './Containers/Auth/Logout/Logout';
+import * as actions from './store/actions/index';
 
 class App extends Component {
+  componentDidMount(){
+    this.props.onTryAutoSignup()
+  }
+
   render(){
+    let routes=(
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Route path="/checkout" component={Checkout} />
+
+        <Redirect to="/" />
+      </Switch>
+    )
+    
+    if(this.props.isAuthenticated){
+      routes=(
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/auth" component={Auth} />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/" />
+        </Switch>
+      )
+    }
+    
     return (
       <div>
         <Layout>
-            <Route path="/" exact component={BurgerBuilder} />
-            <Route path="/orders" exact component={Orders} />
-            <Route path="/auth" exact component={Auth} />
-            <Route path="/logout" exact component={Logout} />
-            <Route path="/checkout" component={Checkout} />
+          {routes}
         </Layout>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps=state=>{
+  return{
+    isAuthenticated:state.auth.token!==null
+  }
+}
+
+const mapDispatchToProps=dispatch=>{
+    return{
+      onTryAutoSignup:()=>dispatch(actions.authCheckState())
+    }
+}
+
+export default withRouter(connect(mapStateToProps,mapDispatchToProps)(App));
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -31,6 +31,9 @@ export const checkAuthTimeout=(expirationTime)=>{
 }
 
 export const logout=()=>{
+    localStorage.removeItem('token')
+    localStorage.removeItem('expirationDate')
+    localStorage.removeItem('userId')
     return{
         type:actionTypes.AUTH_LOGOUT
     }
@@ -44,13 +47,17 @@ export const auth=(email,password,isSignUp)=>{
             password:password,
             returnSecureToken:true
         }
-        let url='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAtCX8SPXKOfVAJ7TFGXK_89TxqmCZQfkI'
+        let url='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key='+process.env.REACT_APP_AUTH_API_KEY
         if(!isSignUp){
-            url='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAtCX8SPXKOfVAJ7TFGXK_89TxqmCZQfkI'
+            url='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key='+process.env.REACT_APP_AUTH_API_KEY
         }
         axios.post(url,authData)
         .then(response=>{
-            dispatch(authSuccess(response.data.idToken,response.data.localId));
+            const expirationDate = new Date(new Date().getTime() + response.data.expiresIn*1000)
+            localStorage.setItem('token',response.data.idToken)
+            localStorage.setItem('expirationDate',expirationDate)
+            localStorage.setItem('userId',response.data.localId)
+            dispatch(authSuccess(response.data.idToken,response.data.localId))
             dispatch(checkAuthTimeout(response.data.expiresIn))
         })
         .catch(err=>{
@@ -59,9 +66,29 @@ export const auth=(email,password,isSignUp)=>{
     }
 }
 
-export const setAuthRedirectPath=(path=>{
+export const setAuthRedirectPath=path=>{
     return{
         type:actionTypes.SET_AUTH_REDIRECT_PATH,
         path:path
     }
-})
+}
+
+export const authCheckState=()=>{
+    return dispatch=> {
+        const token=localStorage.getItem('token')
+        if(!token){
+            dispatch(logout())
+        }
+        else{
+            const expirationDate=new Date(localStorage.getItem('expirationDate'))
+            if(expirationDate>new Date()){
+                const userId=localStorage.getItem('userId')
+                dispatch(authSuccess(token,userId))
+                dispatch(checkAuthTimeout((expirationDate.getTime()-new Date().getTime())/1000))
+            }
+            else{
+                dispatch(logout())
+            }
+        }
+    }
+}
