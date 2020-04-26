40354b12f2fe887ca1b8d3d40ba0484db402a8ce
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-04-27 01:02:20 +0530

    Add NavigationItems.test.js

---
+++ src/Components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {configure,shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import NavigationItems from './NavigationItems';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+configure({adapter:new Adapter()});
+
+describe('<NavigationItems />',()=>{
+    let wrapper;
+    beforeEach(()=>{
+        wrapper=shallow(<NavigationItems />);
+    });
+    it('should render two <NavigationItem /> if not authenticated',()=>{
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+    it('should render three <NavigationItem /> if authenticated',()=>{
+        wrapper.setProps({isAuthenticated:true})
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+    it('should render <NavigationItem logout /> if authenticated',()=>{
+        wrapper.setProps({isAuthenticated:true})
+        expect(wrapper.contains(<NavigationItem link="/logout" exact >Logout</NavigationItem>)).toEqual(true);
+    });
+})
